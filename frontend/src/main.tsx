742c4f010af5c0c548350167cfb40af8df69a91d
vijayvyawhare <vijay.vyawhare>
2025-09-14 14:29:05 +0530

    Clean up Material-UI imports in main.tsx
    - Remove @mui/material/styles and @mui/icons-material imports from main.tsx
    - These imports were moved to App.tsx for better organization
    - Part of the Material-UI CSS bundling fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import '@mui/material/styles'
-import '@mui/icons-material'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
